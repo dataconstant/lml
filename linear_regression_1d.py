31a5adad0b877c29967f54019f341764876534bc
dataconstant <dataconstant@users.noreply.github.com>
2016-10-16 09:59:58 +0530

    Add files via upload

---
+++ linear_regression_1d.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#loading data
+
+df=pd.read_csv('data_1d.csv',header=-1,names=['x','y'])
+x=df['x']
+y=df['y']
+x=np.array(x)
+y=np.array(y)
+
+plt.scatter(x,y)
+plt.show()
+
+den=x.dot(x)-x.mean()*x.sum()
+a=(x.dot(y)-y.mean()*x.sum())/den
+b=(y.mean()*x.dot(x)-x.mean()*x.dot(y))/den
+
+# predicted y
+
+ypred=a*x+b
+
+# plotting results
+
+plt.scatter(x,y)
+plt.plot(x,ypred)
+plt.show()
+
+#checking prediction
+
+d1 = y - ypred
+d2 = y - y.mean()
+r2 = 1 - d1.dot(d1) / d2.dot(d2)
+print("the r-squared is:", r2)
+
